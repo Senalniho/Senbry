c7cef7ac8a9fa2144b9b1ed6b28585642b9d531e
Senanu Adjabeng <adjabengsenanu@yahoo.com>
2024-08-03 13:03:47 +0200

    initial author routes

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+// app.use(bodyParser.json());
+// app.use(methodOverride("_method"));
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -25,4 +30,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+// app.use("/books", bookRouter);
+
 app.listen(process.env.PORT || 5000);
